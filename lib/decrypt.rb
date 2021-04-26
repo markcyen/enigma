1a7620de93b7f69609bdf5219067c17884a69591
MarkY <markcyen@yahoo.com>
2021-04-26 14:10:22 -0600

    Create decrypt file to decrypt encrypted text file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,14 +2,17 @@ require_relative 'enigma'
 
 handle = File.open(ARGV[0], "r")
 
-incoming_message = handle.read
+key = ARGV[2].to_s
+date = ARGV[3].to_s
+
+encrypted_message = handle.read
 enigma = Enigma.new
-decrypt_message = enigma.decrypt(incoming_message)
+decrypt_message = enigma.decrypt(encrypted_message, key, date)
 
 handle.close
 
 writer = File.open(ARGV[1], "w")
 
-writer.write(encrypt_message)
+writer.write(decrypt_message[:decryption])
 
 writer.close
