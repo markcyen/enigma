bb06fa6c5b06b632ce297c685e8ba166f3275256
MarkY <markcyen@yahoo.com>
2021-04-27 06:44:41 -0600

    Add cli message for decrypt runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -10,5 +10,7 @@ writer = File.open(ARGV[1], "w")
 decrypt_message = enigma.decrypt(encrypted_message, ARGV[2].to_s, ARGV[3].to_s)
 writer.write(decrypt_message[:decryption])
 
+puts "Created '#{ARGV[1]}' with the key #{ARGV[2]} and date #{ARGV[3]}"
+
 handle.close
 writer.close
