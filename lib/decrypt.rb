91a5c2c9052d87ada921529e58d79676173ac798
MarkY <markcyen@yahoo.com>
2021-04-26 22:53:35 -0600

    Test for decrypt runner file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,17 +2,13 @@ require_relative 'enigma'
 
 handle = File.open(ARGV[0], "r")
 
-key = ARGV[2].to_s
-date = ARGV[3].to_s
-
 encrypted_message = handle.read
 enigma = Enigma.new
-decrypt_message = enigma.decrypt(encrypted_message, key, date)
-
-handle.close
 
 writer = File.open(ARGV[1], "w")
 
+decrypt_message = enigma.decrypt(encrypted_message, ARGV[2].to_s, ARGV[3].to_s)
 writer.write(decrypt_message[:decryption])
 
+handle.close
 writer.close
